05351946381d14e8a5ff25e6e42ffb97b681b635
BulanCantik <sribulanmegawati@gmail.com>
2017-02-09 15:32:36 +0700

    breadcrumb

--- application/views/pages/member/show.php
+++ application/views/pages/member/show.php
@@ -9,7 +9,7 @@
         Available API
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="<?php echo site_url() ?>member"><i class="fa fa-dashboard"></i> Home</a></li>
         <li><a href="#">Tables</a></li>
         <li class="active">Data tables</li>
       </ol>
